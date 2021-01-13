648f8d6077c2e60e02c76002cd0ccbb4e80d4d4d
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2021-01-13 09:16:17 +0400

    Union in invervals

--- intervaltree-test.py
+++ intervaltree-test.py
@@ -14,15 +14,17 @@ t2[15:25] = 'dgsdf'
 
 print(t1)
 t3 = t1.union(t2)
+for interval_obj in t3:
+    print (interval_obj.begin, ' -- ', interval_obj.end)
 
 #Соединяем интервалы
 #t1 = IntervalTree()
 t3.merge_overlaps()
 #print(set(t3.items()))
-
+print ("Объедияем")
 
 for interval_obj in t3:
-    print (interval_obj)
+    print (interval_obj.begin, ' -- ', interval_obj.end)
 
 
 #for interval in t3: print (interval.begin, ' -- ', interval.end)
